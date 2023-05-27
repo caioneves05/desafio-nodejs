46a228a5de0501a192ee76a27a489f8139b68dd0
Caio Neves <ncaio037@gmail.com>
2023-05-26 23:44:42 -0300

    creating a hash256 for the user's password, and an id with the uuid.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,23 @@
 import User from '../models/User.model.js'
+import cryptojs from 'crypto-js'
+import { v4 } from 'uuid'
 
 class UserController {
     static async createUser (req,res) {
         try{
             const user = new User(req.body)
-            const userCreated = await user.save()
 
-            res.status(201).json(userCreated)
+            const hashedPassword = await cryptojs.SHA256(user.password).toString()
+            user.password = hashedPassword
+            user.id = v4()
+
+            await user.save()
+
+            res.status(201).json({
+                id: `${user.id}`,
+                creat_at: `${user.created_at}`,
+                modified_at: `${user.modified_at}`
+            })
         }
         catch(err) {
             res.status(400).json({ error: err.message })
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -2,10 +2,14 @@ import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema(
     {
+        id: {type: String},
         name: {type: String, required: [true, 'User name is required.']},
         email: {type: String, required: [true, 'User email is required']},
-        password: {type: Number, required: [true, 'User password is required']},
-        telephones: {type: Array}
+        password: {type: String, required: [true, 'User password is required']},
+        telephones: {type: Array},
+
+        created_at: { type: Date, default: Date.now },
+        modified_at: { type: Date, default: Date.now },
     },
     {
         versionKey: false
