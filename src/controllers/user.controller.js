5e7b4d62c112d2a02cc19156a38ab513377d6181
Caio Neves <ncaio037@gmail.com>
2023-05-27 11:49:51 -0300

    implementing user login according to the necessary validations.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,6 +23,37 @@ class UserController {
             res.status(400).json({ error: err.message })
         }
     }
+
+    static async loginUser (req,res) {
+        try{
+            const user = new User(req.body)
+
+            if(user.email && user.password) {
+                const findPassword = await cryptojs.SHA256(user.password).toString()
+                
+                const findUser = await User.findOne({
+                    email: { $eq: user.email },
+                    password : { $eq: findPassword }
+                })
+
+                if(findUser) {
+                    res.status(200).json({
+                        id: findUser.id,
+                        email: findUser.email
+                    })
+                }
+                else {
+                    res.status(401).json({ error: 'Email or password are incorrect' })
+                }
+            } 
+            else {
+                res.status(400).json({message: 'Email or password are required'})
+            }
+
+        }catch(err){
+            res.status(400).json({ error: err.message })
+        }
+    }
 }
 
 export default UserController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router
     .post('/user', UserController.createUser)
+    .get('/user', UserController.loginUser)
 
 
 export default router
